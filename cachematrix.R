f9e2dfc3c5547602f73bac943293cb5379e0531c
Prakruthi P <prakruthi031@gmail.com>
2014-09-21 12:15:04 +0530

    cachematrix.R modified - function makeCacheMatrix.R added and initial comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+There are two functions in this code. The functions together is used to compute the inverse of a matrix. It is also sophisticated to cache the matrix inverse and avoid repeated computation of the inverse of a mtrix.
 
-## Write a short comment describing this function
+The below function is used to get the matrix for inverse computation, set the matrix variable to the given matrix. It is also used to compute the inverse of a matrix and set the value of the computed inverse to the inverse matrix variable.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
